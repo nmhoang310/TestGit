76f883d40792f8f7352da15b5341f57afb50327d
Hoang <nmhoang310@gmail.com>
2015-06-21 09:43:14 +0700

    The beginning

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Hoang
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("This is the beinning");
+
+	}
+
+}
